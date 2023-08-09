5500a79aaf9bb3975d58971ca8585c4d34d873ef
Naqiuddin Kamil <naqiu923@gmail.com>
2023-08-10 01:23:25 +0800

    add mui

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material"
+
+function App() {
+
+  return (
+    <>
+      <Button variant="contained" color="error">Text</Button>
+    </>
+  );
+}
+
+export default App
